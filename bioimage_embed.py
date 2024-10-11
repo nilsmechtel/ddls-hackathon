b38cd1a9b0f7ee0f0308bdedf8ffec8a02e0ce91
Agustin Corbat <agustin.corbat@gmail.com>
2024-10-11 12:57:04 +0200

    Fix dtype in mapping function of bioimage_embed.py

--- bioimage_embed.py
+++ bioimage_embed.py
@@ -1,10 +1,12 @@
-import dask
+# import dask
 import dask.array as da
 from itertools import product
 from pathlib import Path
 from pythae.models import AutoModel
-from skimage import io
+# from skimage import io
 import torch
+
+torch.set_default_device("cuda")
 
 from bioimage_embed import infer
 from bioimage_embed.io import get_tensor_for_row_column
@@ -20,8 +22,8 @@ RESULTS_DIR = Path("./results")
 model = AutoModel.load_from_folder(MODEL_DIR)
 
 # Make dask array of images
-n_rows = 16
-n_cols = 24
+n_rows = 2#16
+n_cols = 1#24
 all_wells = []
 
 for row, col in product(range(1, n_rows + 1), range(1, n_cols + 1)):
@@ -35,12 +37,12 @@ all_wells = all_wells.rechunk((1, 1, 3, 224, 224))
 def run_for_patch(patch):
     assert patch.shape == (1, 1, 3, 224, 224), f"There should be a single well in the input, it was {patch.shape}"
     with torch.no_grad():
-        this_patch = percentile_normalize(torch.Tensor(patch[0, 0]))
+        this_patch = percentile_normalize(torch.Tensor(patch[0, 0].astype("int16")))
         out = infer(model, this_patch[None, ...])["quantized_embed"].squeeze(-1).squeeze(-1).numpy()
     return out[None, ...]
 
 # map all chunks to the function
-out = da.map_blocks(run_for_patch, all_wells, dtype="uint16", drop_axis=[-3, -2, -1], new_axis=2, chunks=(1, 1, 12544))
+out = da.map_blocks(run_for_patch, all_wells, dtype="float32", drop_axis=[-3, -2, -1], new_axis=2, chunks=(1, 1, 12544))
 
 # Compute and save the results to a Zarr file
 out.to_zarr(RESULTS_DIR / "features.zarr", overwrite=True)
