c95514354230946e968c677a833a4de5915b67fa
Agustin Corbat <agustin.corbat@gmail.com>
2024-10-11 13:50:36 +0200

    Run embedding visualization for selected samples

--- embedding_visualization.py
+++ embedding_visualization.py
@@ -12,17 +12,14 @@ DATA_DIR = Path("./data/test")
 
 # Load the features
 features = zarr.open(RESULTS_DIR / "features.zarr", mode="r")
-
-n_rows = 16
-n_cols = 24
-
-index_map = {(row, col): n for n, (row, col) in enumerate(product(range(1, n_rows + 1), range(1, n_cols + 1)))}
+index_map = pd.read_csv(RESULTS_DIR / "selected_wells.csv")
 
 metadata_df = pd.read_csv(DATA_DIR / "PLATEMAP_conf3_labels.csv")
-metadata_df["zarr_index"] = metadata_df.apply(lambda x: index_map[(x["row"], x["col"])], axis=1)
+metadata_df = metadata_df.query("labels != 'samples'").reset_index(drop=True)
+# metadata_df["zarr_index"] = metadata_df.apply(lambda x: index_map[(x["row"], x["col"])], axis=1)
 
 # Calculate UMAP
-concatenated_features = features[metadata_df["zarr_index"].values].reshape((-1, 12544))
+concatenated_features = features[:].reshape((-1, 12544))
 embedding_2d = calculate_umap(concatenated_features, pca_components=50)
 labels = []
 for val in metadata_df["labels"].values:
